0981e3db0fa61a510ca721616d4c61f333f0adf9
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-07-22 17:25:39 -0500

    experience.component is done whit the html in the main page

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-education',
-  templateUrl: './education.component.html',
-  styleUrls: ['./education.component.css']
+  templateUrl: './education.component.html'
 })
-export class EducationComponent implements OnInit {
+export class EducationComponent  {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
