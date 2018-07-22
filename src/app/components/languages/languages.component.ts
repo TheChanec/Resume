bba187738d5d1e6c54f1ac4b0a3a42497983d23c
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-07-22 17:36:27 -0500

    fix in lenguages.component

--- src/app/components/languages/languages.component.ts
+++ src/app/components/languages/languages.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-languages',
-  templateUrl: './languages.component.html',
-  styleUrls: ['./languages.component.css']
+  templateUrl: './languages.component.html'
 })
-export class LanguagesComponent implements OnInit {
+export class LanguagesComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
