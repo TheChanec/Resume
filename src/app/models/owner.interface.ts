483e1a5687677b2bc938b2652fdedce09bb55463
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-07-27 19:54:04 -0500

    Create OwnerInterace

---
+++ src/app/models/owner.interface.ts
@@ -0,0 +1,9 @@
+import { Direccion } from './direccion.Interface';
+
+export interface Owner {
+  Nombre?: string;
+  Bithday?: string;
+  Apellido1?: string;
+  Apellido2?: string;
+  Direccion?: Direccion;
+}
