62f5f0c50ffc7206d97b8892672e82a230719421
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-07-27 19:52:54 -0500

    Import About me service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AboutMeService } from './services/about-me.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private about: AboutMeService) {
+  }
 }
